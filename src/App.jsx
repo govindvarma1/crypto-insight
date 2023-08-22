a105f8fc82a64559086fb5e262699e3805bda7e1
Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2023-08-22 17:51:34 +0530

    implemented lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
 import "./styles.css";
-import NavBar from "./components/Header";
-import CryptoStats from "./pages/CryptoStats";
-import Cryptos from "./pages/cryptocurrencies";
-import CoinDetails from "./pages/coinDetails";
-import Footer from "./components/Footer";
-import News from "./pages/news";
+const NavBar = lazy(() => import("./components/Header"));
+const CryptoStats = lazy(() => import("./pages/CryptoStats"));
+const Cryptos = lazy(() => import("./pages/cryptocurrencies"));
+const CoinDetails = lazy(() => import("./pages/coinDetails"));
+const Footer = lazy(() => import("./components/Footer"));
+const News = lazy(() => import("./pages/news"));
 
 export default function App() {
   return (
     <>
       <NavBar />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<CryptoStats />} />
-          <Route path="/cryptocurrencies" element={<Cryptos />} />
-          <Route path="/cryptocurrencies/:uuid" element={<CoinDetails />} />
-          <Route path="/news" element={<News />} />
-        </Routes>
-      </div>
+      <Suspense fallback={<></>}>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<CryptoStats />} />
+            <Route path="/cryptocurrencies" element={<Cryptos />} />
+            <Route path="/cryptocurrencies/:uuid" element={<CoinDetails />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </div>
+      </Suspense>
       <Footer />
     </>
   );
